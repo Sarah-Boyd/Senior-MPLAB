19e4fadb26627d456d85abf816fd878d7bdc7688
robynmere <98792931+robynmere@users.noreply.github.com>
2024-04-10 20:28:32 -0300

    changed the main code for the actual main code

---
+++ real_main_code3.X/build/default/debug/main_code3.o.d
@@ -0,0 +1,12 @@
+build/default/debug/main_code3.o: main_code3.c config.h i2c.h adc.h imu.h \
+ fuel_gauge.h
+
+config.h:
+
+i2c.h:
+
+adc.h:
+
+imu.h:
+
+fuel_gauge.h:
---
+++ real_main_code3.X/build/default/production/main_code3.o.d
@@ -0,0 +1,12 @@
+build/default/production/main_code3.o: main_code3.c config.h i2c.h adc.h \
+ imu.h fuel_gauge.h
+
+config.h:
+
+i2c.h:
+
+adc.h:
+
+imu.h:
+
+fuel_gauge.h:
